49133be2b45ecde8699a524cd4faa3f093dbe2d3
ruizhang <ruizhang1995@tamu.edu>
2019-01-10 22:59:40 -0600

    Add new game or play again option

--- colorGame.js
+++ colorGame.js
@@ -3,7 +3,23 @@ var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelectorAll("#message") ;
+var messageDisplay = document.querySelector("#message") ;
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors =  generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -14,7 +30,9 @@ for(var i = 0; i < squares.length; i++){
 
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -53,5 +71,5 @@ function randomColor(){
 	var r = Math.floor(Math.random()*256);
 	var g = Math.floor(Math.random()*256);;
 	var b = Math.floor(Math.random()*256);;
-	return "rgb(" + r + "," + g + "," + b + ")";
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
